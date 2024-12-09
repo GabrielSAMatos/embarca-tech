d6afcc5e6d69e268c35033fddc63dca48087f8b0
Gabriel Matos <matos7771@hotmail.com>
2024-12-09 09:14:40 -0300

    varias funcionalidades de funcoes em C

---
+++ Introduction-C/Giraffe/first.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+
+
+
+int poww()
+{
+    printf("%f", pow(2, 3));
+
+    return 0;
+}
+
+int sdfc()
+{
+    int favInt = 7;
+    double favDoub = 7.7;
+    char favChar = 'g';
+
+    printf("My favorite %s is %d and %f\n", "number", favInt, favDoub);
+    printf("and my favorite char is %c", favChar);
+
+
+    return 0;
+}
+
+int dataTypes()
+{
+    int age = 25;
+    double gpa = 3.6;
+    char grade = 'G';
+    char name[] = "Gabriel Matos";
+
+return 0;
+}
+
+int variable()
+{
+    char name[] = "Grabiel";
+    int age = 24;
+
+    printf("There once was a man named %s\n", name);
+    printf("he was %d years old\n", age);
+    age = 25;
+    printf("He really liked the name %s\n", name);
+    printf("but did not like being %d.\n", age);
+
+    return 0;
+}
+
+int triangle()
+{
+    printf("   /|\n");
+    printf("  / |\n");
+    printf(" /  |\n");
+    printf("/___|\n");
+
+    return 0;
+}
