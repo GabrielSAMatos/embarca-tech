11478aeec22fb31368151a52c19423c5df1861df
Gabriel Matos <matos7771@hotmail.com>
2024-12-09 15:31:37 -0300

    calling a function

--- Introduction-C/Giraffe/first.c
+++ Introduction-C/Giraffe/first.c
@@ -3,7 +3,75 @@
 #include <math.h>
 
 
-int scanffgets()
+int main()
+{
+
+    return 0;
+}
+
+int callFunction()
+{
+
+    sayHi("Grabiel", 25);
+    sayHi("Matos", 25);
+    sayHi("Jon", 99);
+
+
+}
+
+void sayHi(char name[], int age)
+{
+
+    printf("Hello, %s. You are %d years old\n", name, age);
+}
+
+void array()
+{
+
+    int luckyNumbers[10];
+
+    luckyNumbers[1] = 20;
+
+    printf("%d", luckyNumbers[0]);
+
+}
+
+void twoParametersInScanf()
+{
+
+    char color[20], plural[20], celebrityF[20], celebrityL[20];
+
+    printf("Enter an color: ");
+    scanf("%s", color);
+
+    printf("Enter an plural-noun: ");
+    scanf("%s", plural);
+    
+    printf("Enter an celebrity: ");
+    scanf("%s %s", celebrityF, celebrityL);
+
+    printf("\n");
+    printf("Roses are %s\n", color);
+    printf("%s are blue\n", plural);
+    printf("I love %s %s\n", celebrityF, celebrityL);
+
+}
+
+void summ()
+{
+    double num1, num2;
+
+    printf("Enter first number: ");
+    scanf("%lf", &num1);
+
+    printf("Enter second number: ");
+    scanf("%lf", &num2);
+
+    printf("The sum of %lf and %lf is: %lf", num1, num2, num1 + num2);
+
+}
+
+void scanffgets()
 {
     int age;
     double gpa;
@@ -28,33 +96,28 @@ int scanffgets()
     printf("You are %d years old.\n", age);
     printf("Your gpa is %f.", gpa);
 
-    return 0;
 }
 
-
-
-int constvariable(){
+void constvariable(){
 
     const int num = 5;
     printf("%d\n", num);
     //num = 8;
     printf("%d", num);
 
-    return 0;
 }
 /*
     Comentarios em C #fe
 */
-int powsqrt()
+void powsqrt()
 {
     printf("%f \n", pow(2, 3));
     printf("%f", sqrt(4));
     //ceil arredonda pra cima
     //floor arredonda pra baixo
-    return 0;
 }
 
-int sdfc()
+void sdfc()
 {
     int favInt = 7;
     double favDoub = 7.7;
@@ -64,20 +127,17 @@ int sdfc()
     printf("and my favorite char is %c", favChar);
 
 
-    return 0;
 }
 
-int dataTypes()
+void dataTypes()
 {
     int age = 25;
     double gpa = 3.6;
     char grade = 'G';
     char name[] = "Gabriel Matos";
-
-return 0;
 }
 
-int variable()
+void variable()
 {
     char name[] = "Grabiel";
     int age = 24;
@@ -88,15 +148,13 @@ int variable()
     printf("He really liked the name %s\n", name);
     printf("but did not like being %d.\n", age);
 
-    return 0;
 }
 
-int triangle()
+void triangle()
 {
     printf("   /|\n");
     printf("  / |\n");
     printf(" /  |\n");
     printf("/___|\n");
 
-    return 0;
 }
