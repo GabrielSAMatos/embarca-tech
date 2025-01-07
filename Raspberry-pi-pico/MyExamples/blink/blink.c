6e368ec68c35449109851b2763b4ae7eb204badf
Gabriel Matos <matos7771@hotmail.com>
2025-01-07 20:07:09 -0300

    variavel trocada por #define, e agora o led pisca nao so em azul, como tambem vermelho e verde

--- Raspberry-pi-pico/MyExamples/blink/blink.c
+++ Raspberry-pi-pico/MyExamples/blink/blink.c
@@ -3,32 +3,37 @@
 //biblioteca guarda-chuva, possui varias bibliotecas para acessar pinos I/O, temporizadores etc
 #include "pico/stdlib.h"
 
-//define que sera usado o pino GP12
-const uint led_pin_blue = 12;
+//define o valor das cores que irao ser usadas no led
+// 11 verde, 12 azul, 13 vermelho
+#define LED_R_PIN 13
+#define LED_G_PIN 11
+#define LED_B_PIN 12
+
+
+int blink(int color){
+    gpio_put(color, true);
+    sleep_ms(1000);
+    gpio_put(color, false);
+    sleep_ms(1000);
+    return 0;
+}
 
 int main(){
-    uint a = 0;
+    //os dois configuram o pino GPIO como uma saida
+    gpio_init(LED_R_PIN);
+    gpio_init(LED_G_PIN);
+    gpio_init(LED_B_PIN);
 
-    //os dois configuram o pino GP12 como uma saida
-    gpio_init(led_pin_blue);
-    gpio_set_dir(led_pin_blue, GPIO_OUT);
+    gpio_set_dir(LED_R_PIN, GPIO_OUT);
+    gpio_set_dir(LED_G_PIN, GPIO_OUT);
+    gpio_set_dir(LED_B_PIN, GPIO_OUT);
+
     //faz a configuracao basica de entrada e saida
     stdio_init_all();
     
     while(true){
-        gpio_put(led_pin_blue, true);
-        sleep_ms(1800);
-        gpio_put(led_pin_blue, false);
-        sleep_ms(200);
-        a++;
-        printf("%d\r\n", a);
-        if(a == 5){
-            break;
-        }
+        blink(LED_R_PIN);
+        blink(LED_G_PIN);
+        blink(LED_B_PIN);
     }
-    printf("digita ai o a: ");
-    scanf("%d", &a);
-    printf("its over baby\n");
-    printf("toma aqui o a: %d", a);
-
 }
