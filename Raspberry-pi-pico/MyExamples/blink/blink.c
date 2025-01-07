1f1c1710b4df6f3c8f47e0d934cfbb02e66baad8
Gabriel Matos <matos7771@hotmail.com>
2025-01-07 17:37:57 -0300

    nome da variavel trocada de red para blue e um scanf adicionado, porem nao funciona em tese

--- Raspberry-pi-pico/MyExamples/blink/blink.c
+++ Raspberry-pi-pico/MyExamples/blink/blink.c
@@ -4,24 +4,31 @@
 #include "pico/stdlib.h"
 
 //define que sera usado o pino GP12
-const uint led_pin_red = 12;
+const uint led_pin_blue = 12;
 
 int main(){
     uint a = 0;
 
     //os dois configuram o pino GP12 como uma saida
-    gpio_init(led_pin_red);
-    gpio_set_dir(led_pin_red, GPIO_OUT);
+    gpio_init(led_pin_blue);
+    gpio_set_dir(led_pin_blue, GPIO_OUT);
     //faz a configuracao basica de entrada e saida
     stdio_init_all();
     
     while(true){
-        gpio_put(led_pin_red, true);
+        gpio_put(led_pin_blue, true);
         sleep_ms(1800);
-        gpio_put(led_pin_red, false);
+        gpio_put(led_pin_blue, false);
         sleep_ms(200);
-
-        printf("%d\r\n", a);
         a++;
+        printf("%d\r\n", a);
+        if(a == 5){
+            break;
+        }
     }
+    printf("digita ai o a: ");
+    scanf("%d", &a);
+    printf("its over baby\n");
+    printf("toma aqui o a: %d", a);
+
 }
